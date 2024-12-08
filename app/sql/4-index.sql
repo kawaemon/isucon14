f157105b3b2d96b3910643a17e37b5bfb7c39f5d
kawaemon <me@kawaemon.dev>
2024-12-08 10:46:49 +0900

    index more

--- app/sql/4-index.sql
+++ app/sql/4-index.sql
@@ -1,1 +1,6 @@
 ALTER TABLE chairs ADD INDEX idx_access_token (access_token);
+
+alter table rides add index idx_chair_id(chair_id);
+alter table ride_statuses add index idx_ride_id(ride_id);
+alter table ride_statuses add index idx_ride_id_and_app_sent_at(ride_id, app_sent_at);
+alter table chair_locations add index idx_chair_id(chair_id);
