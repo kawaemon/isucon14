e6ed7665208d99d2a4e73bf7208869823b31546b
kawaemon <me@kawaemon.dev>
2024-12-08 17:23:21 +0900

    try

--- app/go/app_handlers.go
+++ app/go/app_handlers.go
@@ -730,7 +730,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-				RetryAfterMs: 30,
+				RetryAfterMs: 125,
 			})
 			return
 		}
@@ -777,7 +777,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 			CreatedAt: ride.CreatedAt.UnixMilli(),
 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
 		},
-		RetryAfterMs: 30,
+		RetryAfterMs: 125,
 	}
 
 	if ride.ChairID.Valid {
