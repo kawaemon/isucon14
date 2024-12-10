432261d195120784f9df5846996617581cad1d58
kawaemon <me@kawaemon.dev>
2024-12-10 20:53:11 +0900

    debug

--- app/rust/src/internal_handlers.rs
+++ app/rust/src/internal_handlers.rs
@@ -21,22 +21,27 @@ async fn internal_get_matching(
             .fetch_all(&pool)
             .await?;
 
+    let rides_count = rides.len();
+    let mut matches = 0;
+    let mut trials = 0;
+
     for ride in rides {
         for _ in 0..10 {
+            trials += 1;
             let Some(matched): Option<Chair> =
             sqlx::query_as("SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1")
                 .fetch_optional(&pool)
                 .await?
-        else {
-            return Ok(StatusCode::NO_CONTENT);
-        };
+            else {
+                return Ok(StatusCode::NO_CONTENT);
+            };
 
             let empty: bool = sqlx::query_scalar(
-            "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE",
-        )
-        .bind(&matched.id)
-        .fetch_one(&pool)
-        .await?;
+                "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE",
+            )
+            .bind(&matched.id)
+            .fetch_one(&pool)
+            .await?;
 
             if empty {
                 sqlx::query("UPDATE rides SET chair_id = ? WHERE id = ?")
@@ -44,9 +49,17 @@ async fn internal_get_matching(
                     .bind(ride.id)
                     .execute(&pool)
                     .await?;
+                matches += 1;
                 break;
             }
         }
+    }
+
+    if rides_count > 0 {
+        tracing::info!(
+            "matching: waiting={rides_count}, matched {matches} with {trials} trials => {:.02}",
+            (trials as f64) / (matches as f64)
+        );
     }
 
     Ok(StatusCode::NO_CONTENT)
