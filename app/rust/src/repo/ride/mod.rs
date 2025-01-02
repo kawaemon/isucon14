146e163fdb46f2039e2a0f5e074d15b66b931488
kawaemon <me@kawaemon.dev>
2025-01-03 00:03:51 +0900

    no?

--- app/rust/src/repo/ride/mod.rs
+++ app/rust/src/repo/ride/mod.rs
@@ -647,7 +647,7 @@ impl Repository {
                     .iter()
                     .enumerate()
                     .filter(|(_i, x)| x.coord.distance(ride.pickup) < 10 * x.speed)
-                    .min_by_key(|(_i, x)| x.coord.distance(ride.pickup))
+                    .max_by_key(|(_i, x)| x.coord.distance(ride.pickup))
                 else {
                     redu.push((ride, status_id));
                     continue;
